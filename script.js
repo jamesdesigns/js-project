0d8a05bfc5f619b5d18a2f552d658f197687b30e
James Hooper <jameshooperdesigns@gmail.com>
2020-04-17 22:46:51 -0600

    Add titles for each video

--- script.js
+++ script.js
@@ -114,12 +114,27 @@ logoSize();
 //                 })
 //                 .catch(err => console.error(err));
 
-function witcher(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i<arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' + 
-        arr[i].display + '</a><br>';
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to a more compatible method here
     }
-    document.getElementById("one").innerHTML = out;
-    }
+  });
