07f39de9211e8a16c4ce5ab64e29f0047ce43b25
James Hooper <jameshooperdesigns@gmail.com>
2020-12-15 22:36:51 -0700

    Add Like feature to the characters

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // "use strict";
+var counter = 0;  
 
 // JavaScript code 
 function search_character() { 
@@ -46,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // This controls the Like Buttons
   function myFunction(x) {
 	x.classList.toggle("fa-heartbeat");
+	document.querySelector('.favorite').innerHTML = counter += 1;
   }
 
   // This controls the menu
@@ -58,92 +60,3 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("myNav").style.width = "0%";
 	document.getElementById("myNav").style.transitionDuration = ".5s";
   }
-
-
-// fetch('./witcher.json')
-//                 .then(res => res.json())
-//                 .then(data => {
-//                   console.log(data.name)
-//                 })
-//                 .catch(err => console.error(err));
-
-/* lazyload.js (c) Lorenzo Giuliani
- * MIT License (http://www.opensource.org/licenses/mit-license.html)
- *
- * expects a list of:  
- * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
- */
-
-// !function(window){
-//     var $q = function(q, res){
-//           if (document.querySelectorAll) {
-//             res = document.querySelectorAll(q);
-//           } else {
-//             var d=document
-//               , a=d.styleSheets[0] || d.createStyleSheet();
-//             a.addRule(q,'f:b');
-//             for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
-//               l[b].currentStyle.f && c.push(l[b]);
-  
-//             a.removeRule(0);
-//             res = c;
-//           }
-//           return res;
-//         }
-//       , addEventListener = function(evt, fn){
-//           window.addEventListener
-//             ? this.addEventListener(evt, fn, false)
-//             : (window.attachEvent)
-//               ? this.attachEvent('on' + evt, fn)
-//               : this['on' + evt] = fn;
-//         }
-//       , _has = function(obj, key) {
-//           return Object.prototype.hasOwnProperty.call(obj, key);
-//         }
-//       ;
-  
-//     function loadImage (el, fn) {
-//       var img = new Image()
-//         , src = el.getAttribute('data-src');
-//       img.onload = function() {
-//         if (!! el.parent)
-//           el.parent.replaceChild(img, el)
-//         else
-//           el.src = src;
-  
-//         fn? fn() : null;
-//       }
-//       img.src = src;
-//     }
-  
-//     function elementInViewport(el) {
-//       var rect = el.getBoundingClientRect()
-  
-//       return (
-//          rect.top    >= 0
-//       && rect.left   >= 0
-//       && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-//       )
-//     }
-  
-//       var images = new Array()
-//         , query = $q('img.lazy')
-//         , processScroll = function(){
-//             for (var i = 0; i < images.length; i++) {
-//               if (elementInViewport(images[i])) {
-//                 loadImage(images[i], function () {
-//                   images.splice(i, i);
-//                 });
-//               }
-//             };
-//           }
-//         ;
-//       // Array.prototype.slice.call is not callable under our lovely IE8 
-//       for (var i = 0; i < query.length; i++) {
-//         images.push(query[i]);
-//       };
-  
-//       processScroll();
-//       addEventListener('scroll',processScroll);
-  
-//   }(this);
